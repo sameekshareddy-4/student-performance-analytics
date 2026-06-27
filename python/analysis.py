2a3e5a25e52c5376e988e42def156ff36b16eba6
Sameeksha Reddy <sameekshadsr0412@gmail.com>
2026-06-27 11:36:51 +0530

    Update analysis.py

--- python/analysis.py
+++ python/analysis.py
@@ -16,8 +16,48 @@ query = "SELECT * FROM Students"
 # Read SQL into DataFrame
 df = pd.read_sql(query, connection)
 
-# Display Data
-# print(df)
+#Display Data
+
+print(df)
 print(df.head())
+print(df.tail())
+print(df.shape)
+print(df.columns)
+print(df.dtypes)
+print(df.info())
+print(df.describe())
+print(len(df))
+print(df["branch"].unique())
+print(df["branch"].value_counts())
+
+print(df["student_name"])
+print(df[["student_name","branch"]])
+print(df[df["branch"]=="CSE"])
+print(df[df["gender"]=="Female"])
+print(df[df["age"]>20])
+
+print(
+df[
+(df["gender"]=="Female") &
+(df["branch"]=="CSE")
+]
+)
+print(
+df[
+(df["gender"]=="Female") |
+(df["branch"]=="AIML")
+]
+)
+
+print(df.sort_values("age",ascending=False))
+print(df.groupby("branch").size())
+print(
+df.groupby("branch")["age"].mean()
+)
+print(
+df.groupby("branch")["age"].agg(
+["mean","max","min"]
+)
+)
 
 connection.close()
